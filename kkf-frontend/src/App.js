0181dae1f0635d3e974d11f1c75e090bcdb948a9
taylorsieling <taylorasieling@gmail.com>
2021-06-03 20:20:01 -0500

    creating frontend

--- kkf-frontend/src/App.js
+++ kkf-frontend/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/Home'
+// import Navigation from './components/Navigation'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+
+      <Router>
+
+        {/* <div className="navbar">
+        <Navigation />
+        </div> */}
+        
+          <Switch>
+            <Route exact path='/' component={ Home }/>
+          </Switch>
+
+        {/* <div className="footer">
+          <Footer/>
+        </div> */}
+
+      </Router>
+      
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    state
+    // kittens: state.kittenReducer.kittens,
+    // loading: state.kittenReducer.loading
+  }
+}
+
+export default connect(mapStateToProps)(App);
--- kkf-frontend/src/index.js
+++ kkf-frontend/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/rootReducer'
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
